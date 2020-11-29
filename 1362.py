f82f2cb1d67197f5217379a7b17e923b6ea8a13d
William Almeida <william.almeida@usp.br>
2020-11-29 02:47:14 -0300

    Update 1362.py

--- 1362.py
+++ 1362.py
@@ -1,47 +1,52 @@
-def camisa_to_n(text):
-    if (s == "XS"):
+def calcula(p):
+    global encontrado
+    if p == int(m)+1:
+        encontrado = True
+        return
+    if encontrado:
+        return
+    if quantidade[tipo[p][0]]:
+        quantidade[tipo[p][0]] -= 1
+        calcula(p + 1)
+        quantidade[tipo[p][0]] += 1
+    if quantidade[tipo[p][1]]:
+        quantidade[tipo[p][1]] -= 1
+        calcula(p + 1)
+        quantidade[tipo[p][1]] += 1
+
+
+def valor(tamanho):
+    if tamanho == "XS":
         return 1
-	if (s == "S"):
+    if tamanho == "S":
         return 2
-	if (s == "M"):
+    if tamanho == "M":
         return 3
-	if (s == "L"):
+    if tamanho == "L":
         return 4
-	if (s == "XL"):
-        return 5;
-	return 6;
-
-def verificar(x):
-    if (x==m):
-        check=True
-        return True
-    if check:
-        return True
-    if disp[tam[x][0]]>0:
-        disp[tam[x][0]]-=1
-		verificar(x + 1)
-		disp[tam[x][0]]+=1
-    if (disp[tam[x][1]]>0):
-		disp[tam[x][1]]-=1
-		verificar(x + 1)
-		disp[tam[x][1]]+=1
+    if tamanho == "XL":
+        return 5
+    return 6
 
 
-n_testes=int(input())
-disp=[0]*10
-tam=[[0]*2]*40
-while(n_testes!=0):
-    text=input()
-    text_list=text.split(" ")
-    n=int(text_list[0])
-    m=int(text_list[1])
-    for i in range(len(disp)):
-        disp[i]=n/6
-    for i in range(0,m):
-        camisas=input()
-        camisas_list=camisas.split(" ")
-        tam[i][0]=camisa_to_n(camisas_list[0])
-        tam[i][1]=camisa_to_n(camisas_list[1])
-    check=False
-    verificar(0)
-    n_tests-=1
+quantidade = [0] * 10
+tipo = [[0 for i in range(2)] for j in range(40)]
+
+casos = int(input())
+
+for caso in range(casos):
+    n, m = input().split()
+    for i in range(10):
+        quantidade[i] = int(n)/6;
+    for i in range(1, int(m)+1):
+        s1, s2 = input().split()
+        tipo[i][0] = valor(s1)
+        tipo[i][1] = valor(s2)
+
+    encontrado = False
+    calcula(1)
+
+    if encontrado:
+        print("YES")
+    else:
+        print("NO")
