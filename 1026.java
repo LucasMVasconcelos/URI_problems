34ca0214726335719a19ae9158f1b8dcd317c34c
William Almeida <william.almeida@usp.br>
2020-11-28 22:28:34 -0300

    Update 1026.java

--- 1026.java
+++ 1026.java
@@ -10,12 +10,12 @@ public class Main {
             num2 = Long.toBinaryString(scan.nextLong());
 	        String total = "";
 	        for (int i = 1; i <= num1.length() || i <= num2.length(); i++) {
-	        	int tot = 0;
-	        	if (i > num1.length()) tot = Character.getNumericValue(num2.charAt(num2.length()-i));
-	        	else if (i > num2.length()) tot = Character.getNumericValue(num1.charAt(num1.length()-i));
-	        	else if (Character.getNumericValue(num1.charAt(num1.length()-i)) != Character.getNumericValue(num2.charAt(num2.length()-i))) tot = 1;
+	        	int temp = 0;
+	        	if (i > num1.length()) temp = Character.getNumericValue(num2.charAt(num2.length()-i));
+	        	else if (i > num2.length()) temp = Character.getNumericValue(num1.charAt(num1.length()-i));
+	        	else if (Character.getNumericValue(num1.charAt(num1.length()-i)) != Character.getNumericValue(num2.charAt(num2.length()-i))) temp = 1;
 	        	
-	        	total = tot+total;
+	        	total = temp+total;
 	        }
 	        System.out.println(Long.parseLong(total,2));
     	}
