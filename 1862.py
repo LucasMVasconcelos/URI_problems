1aef915b8179943bfcffb4946a901b994c74da33
Lucas de Matos Vasconcelos <lucas_vasconselos.11@hotmail.com>
2020-11-28 23:16:42 -0300

    alteração arquivo

--- 1862.py
+++ 1862.py
@@ -1,51 +1,51 @@
 
 
-class Graph:
+class Grafo:
     def __init__(self, V):
         self.V = V
         self.adj = [[] for i in range(V)]
 
 
-    def addRelation(self, v, w):
+    def rel(self, v, w):
         v -= 1
         w -= 1
         self.adj[v].append(w)
         self.adj[w].append(v)
 
-    def countUtil(self, v, visited):
-        count = 1
-        visited[v] = True
+    def contaUtil(self, v, visitados):
+        conta = 1
+        visitados[v] = True
         i = 0
         while i != len(self.adj[v]):
-            if (not visited[self.adj[v][i]]):
-                count = count + self.countUtil(self.adj[v][i],
-                                                      visited)
+            if (not visitados[self.adj[v][i]]):
+                conta = conta + self.contaUtil(self.adj[v][i],
+                                                      visitados)
             i += 1
-        return count
+        return conta
 
-    def countGroups(self):
+    def contaGrupos(self):
 
 
-        visited = [0] * self.V
-        n_people=[]
-        existing_groups = 0
-        new_groups = 1
+        visitados = [0] * self.V
+        n_pessoas=[]
+        e_grupos = 0
+        grupos = 1
         for i in range(self.V):
 
             # If not in any group.
-            if (visited[i] == False):
-                existing_groups += 1
-                n_people.append(self.countUtil(i, visited))
-                new_groups = (new_groups *
-                              self.countUtil(i, visited))
+            if (visitados[i] == False):
+                e_grupos += 1
+                n_pessoas.append(self.contaUtil(i, visitados))
+                grupos = (grupos *
+                              self.contaUtil(i, visitados))
 
-        if (existing_groups == 1):
-            new_groups = 0
-        if (existing_groups == 1):
-            existing_groups = -1
+        if (e_grupos == 1):
+            grupos = 0
+        if (e_grupos == 1):
+            e_grupos = -1
 
 
-        return existing_groups,n_people
+        return e_grupos,n_pessoas
 
 
 
@@ -54,11 +54,11 @@ houses = []
 for _ in range(n):
     houses.append(input())
 houses_target=[[1 if l=='S' else 0 for l in i] for i in houses]
-g = Graph(n)
+g = Grafo(n)
 for linha in range(n):
     for coluna in range(n):
         if houses_target[linha][coluna]==1 and linha!=coluna:
-            g.addRelation(linha, coluna)
+            g.rel(linha, coluna)
 n_groups,n_people_group=g.countGroups()
 n_people_group.sort(reverse=True)
 n_people_group_text=[str(i) for i in n_people_group]
